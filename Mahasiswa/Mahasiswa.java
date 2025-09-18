5ec1828da8821b9885711cf420c25c25fa905898
triontin06-rgb <triontin06@gmail.com>
2025-09-18 14:08:56 +0700

    pembuatan file

---
+++ Mahasiswa/Mahasiswa.java
@@ -0,0 +1,22 @@
+package Mahasiswa;
+
+public class Mahasiswa {
+    public String nama;
+    public String nim;
+    public String jurusan;
+    public double ipk;
+
+    public Mahasiswa(String nama, String nim, String jurusan, double ipk){
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+        this.ipk = ipk;
+    }
+
+    public void ShowMahasiswa(){
+        System.out.println("nama: " + this.nama);
+        System.out.println("Gaji: " + this.nim);
+        System.out.println("jurusan: " + this.jurusan);
+        System.out.println("ipk: " + this.ipk);
+    }
+}
---
+++ Mahasiswa/MahasiswaTest.java
@@ -0,0 +1,13 @@
+public class MahasiswaTest{
+    
+    public static void main(String[] args) {
+        Mahasiswa Mahasiswa1 = new Mahasiswa("Triontin", "2403061", "Teknik Informatika", 4 );
+        Mahasiswa1.ShowMahasiswa();
+
+        Mahasiswa Mahasiswa2 = new Mahasiswa("Lutfiah", "2403032", "Keperawatan", 4 );
+        Mahasiswa2.ShowMahasiswa();
+
+        Mahasiswa Mahasiswa3 = new Mahasiswa("Virna", "2403067", "RPL", 4 );
+        Mahasiswa3.ShowMahasiswa();
+    }
+}
