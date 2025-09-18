869ed3aff5f6d896ecc67dea5ec08395557523a7
virnananju <nanjuvivo@gmail.com>
2025-09-18 13:19:12 +0700

    kode matkul
    mengisi matakuliahtest.java

--- MataKuliahTest.java
+++ MataKuliahTest.java
@@ -1,6 +1,12 @@
 public class MataKuliahTest {
 
     public static void main(String[] args) {
+        MataKuliah matkul1 = new MataKuliah("Pemrograman web lanjut", 4);
+        matkul1.showMataKuliah();
+
+        MataKuliah matkul2 = new MataKuliah("Pemrograman berorientasi objek", 3);
+        matkul2.showMataKuliah();
     }
+
     
 }
