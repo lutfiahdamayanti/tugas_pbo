6b9bc353d455911acb316b7f13d093fd78239127
virnananju <nanjuvivo@gmail.com>
2025-09-18 09:56:55 +0700

    perubahan nama file
    nama file Matkul Menjadi MataKuliah dan pengisian property di MataKuliah.java

---
+++ MataKuliah.java
@@ -0,0 +1,17 @@
+public class MataKuliah {
+
+    //property
+    public String namaMatkul;
+    public int sks;
+
+    //method
+    public MataKuliah(String namaMatkul, int sks){
+        this.namaMatkul = namaMatkul; 
+        this.sks = sks; 
+    }
+
+    public void showMataKuliah() {
+
+    }
+    
+}
